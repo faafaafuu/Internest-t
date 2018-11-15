77da79741e9939646bcb698dd438852176521ac9
f <f@MacBook-Pro-f.local>
2018-11-15 21:20:56 +0300

    set options value

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import {goods} from './shop.json'
 import './App.css';
 
 class App extends Component {
+  // state = {
+  //   goods: {goods},
+  //   inputValue: ''
+  // }
   render() {
+    // const {goods, inputValue} = this.state
+    
     return (
       <div className="App"> 
         <Goods goods={goods}/>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../assets/Search.sass'
 
-export default (props) => 
-    (
+export default class Search extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    render() {
+        const optionItem = this.handleSubmit
+        return (
             <div className='container search'>
                 <div className="input-group ">
                     <div className="input-group-prepend">
                         <label 
                             className="input-group-text" 
-                            htmlFor="inputGroupSelect01" 
-                            > Sort
+                            htmlFor="inputGroupSelect01"
+                            onClick={optionItem}
+                            > Sort by
                         </label>
                     </div>
-                    <select className="custom-select" id="inputGroupSelect01">
-                        <option value=''>Choose...</option>
-                        <option defaultValue="1">Price</option>
-                        <option defaultValue="2">Title</option>
-                        <option defaultValue="3">Old Price</option>
+                    <select 
+                        className="custom-select" 
+                        id="inputGroupSelect01" 
+                        value={this.props.value} 
+                        onChange={this.handleChange}>
+
+                        <option value='Choose'>Choose...</option>
+                        <option value="price">Price</option>
+                        <option value="title">Title</option>
+                        <option value="oldPrice">Old Price</option>
                     </select>
                 </div>
                 <form className="form-inline my-3">
@@ -24,4 +41,22 @@ export default (props) =>
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
-    )
+        )
+    }
+    // sortBy = (key) => {
+    //     return key
+    // }
+
+    //получит значение options
+    handleChange(ev) {
+        this.setState({
+            value: ev.target.value
+        });
+    }
+    
+    handleSubmit(ev) {
+        console.log(this.state.value);
+        ev.preventDefault();
+    }
+    
+}
